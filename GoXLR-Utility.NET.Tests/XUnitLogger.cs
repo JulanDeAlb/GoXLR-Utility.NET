0a7016124afa173cc57272753aae22eee440d171
JulanDeAlb <67780894+JulanDeAlb@users.noreply.github.com>
2023-03-04 17:44:15 +0100

    Added temporary Logger to log into Tests output

---
+++ GoXLR-Utility.NET.Tests/XUnitLogger.cs
@@ -0,0 +1,29 @@
+using Microsoft.Extensions.Logging;
+using Xunit.Abstractions;
+
+namespace GoXLR_Utility.NET.Tests;
+
+public class XUnitLogger : ILogger
+{
+    private readonly ITestOutputHelper _output;
+
+    public XUnitLogger(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+    {
+        _output.WriteLine($"Message: {state} | Exception: {exception}");
+    }
+
+    public bool IsEnabled(LogLevel logLevel)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IDisposable? BeginScope<TState>(TState state) where TState : notnull
+    {
+        throw new NotImplementedException();
+    }
+}
