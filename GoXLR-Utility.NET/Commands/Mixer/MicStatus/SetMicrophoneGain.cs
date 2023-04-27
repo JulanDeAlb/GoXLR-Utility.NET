9eee2a83dd7bdaeed09caa86ae9b82d18abc6fdf
JulanDeAlb <67780894+JulanDeAlb@users.noreply.github.com>
2023-04-27 09:53:10 +0200

    Fix SetMicrophoneGain (Copy paste issue)

--- GoXLR-Utility.NET/Commands/Mixer/MicStatus/SetMicrophoneGain.cs
+++ GoXLR-Utility.NET/Commands/Mixer/MicStatus/SetMicrophoneGain.cs
@@ -3,7 +3,7 @@ using GoXLR_Utility.NET.Enums.Response.Status.Mixer.MicStatus;
 
 namespace GoXLR_Utility.NET.Commands.Mixer.MicStatus
 {
-    public class SetGateThreshold : DeviceCommandBase
+    public class SetMicrophoneGain : DeviceCommandBase
     {
         private const int MinValue = 0;
         private const int MaxValue = 72;
@@ -13,9 +13,9 @@ namespace GoXLR_Utility.NET.Commands.Mixer.MicStatus
         /// </summary>
         /// <param name="type">The Type to edit</param>
         /// <param name="gain">Gain as Byte (0 - 72)</param>
-        public SetGateThreshold(MicrophoneType type, byte gain)
+        public SetMicrophoneGain(MicrophoneType type, byte gain)
         {
-            gain = gain > MaxValue ? (byte) SetMaxValue(nameof(SetGateThreshold), MaxValue) : gain;
+            gain = gain > MaxValue ? (byte) SetMaxValue(nameof(SetMicrophoneGain), MaxValue) : gain;
 
             Command = new Dictionary<string, object>
             {
@@ -32,10 +32,10 @@ namespace GoXLR_Utility.NET.Commands.Mixer.MicStatus
         /// </summary>
         /// <param name="type">The Type to edit</param>
         /// <param name="gain">Gain as Int (0 - 72)</param>
-        public SetGateThreshold(MicrophoneType type, int gain)
+        public SetMicrophoneGain(MicrophoneType type, int gain)
         {
-            gain = gain < MinValue ? SetMinValue(nameof(SetGateThreshold), MinValue) : gain;
-            gain = gain > MaxValue ? SetMaxValue(nameof(SetGateThreshold), MaxValue) : gain;
+            gain = gain < MinValue ? SetMinValue(nameof(SetMicrophoneGain), MinValue) : gain;
+            gain = gain > MaxValue ? SetMaxValue(nameof(SetMicrophoneGain), MaxValue) : gain;
 
             Command = new Dictionary<string, object>
             {
