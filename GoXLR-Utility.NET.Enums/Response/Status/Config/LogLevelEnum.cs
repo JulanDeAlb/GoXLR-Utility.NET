6d200ae6c3624fd816f6c70042f389fd08f30df4
JulanDeAlb <67780894+JulanDeAlb@users.noreply.github.com>
2023-06-25 20:48:26 +0200

    Create LogLevelEnum.cs

---
+++ GoXLR-Utility.NET.Enums/Response/Status/Config/LogLevelEnum.cs
@@ -0,0 +1,12 @@
+namespace GoXLR_Utility.NET.Enums.Response.Status.Config
+{
+    public enum LogLevelEnum
+    {
+        Off,
+        Error,
+        Warn,
+        Info,
+        Debug,
+        Trace
+    }
+}
