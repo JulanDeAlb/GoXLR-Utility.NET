116431f3067b449d63aa6e57188502480fd46a77
JulanDeAlb <67780894+JulanDeAlb@users.noreply.github.com>
2023-06-25 22:04:31 +0200

    Create SetGlobalColour.cs

---
+++ GoXLR-Utility.NET.Commands/Mixer/Lighting/SetGlobalColour.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using GoXLR_Utility.NET.Enums.Response.Status.Mixer.Lighting.Button;
+
+namespace GoXLR_Utility.NET.Commands.Mixer.Lighting
+{
+    public class SetGlobalColour : DeviceCommandBase
+    {
+        /// <summary>
+        /// Set the Global Colour.
+        /// </summary>
+        /// <param name="colour">The Colour (#ffffff)</param>
+        public SetGlobalColour(string colour)
+        {
+            colour = colour.Replace("#", "");
+
+            Command = new Dictionary<string, object>
+            {
+                ["SetGlobalColour"] = new object[]
+                {
+                    colour
+                }
+            };
+        }
+    }
+}
