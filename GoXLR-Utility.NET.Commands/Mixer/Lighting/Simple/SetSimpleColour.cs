f1519c30eb8b253166053a4cdbd26f8bc335b937
JulanDeAlb <67780894+JulanDeAlb@users.noreply.github.com>
2023-06-25 22:59:50 +0200

    Make notice that Global does not work here

--- GoXLR-Utility.NET.Commands/Mixer/Lighting/Simple/SetSimpleColour.cs
+++ GoXLR-Utility.NET.Commands/Mixer/Lighting/Simple/SetSimpleColour.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using GoXLR_Utility.NET.Enums.Response.Status.Mixer.Lighting.Simple;
+using Microsoft.Extensions.Logging;
 
 namespace GoXLR_Utility.NET.Commands.Mixer.Lighting.Simple
 {
@@ -7,7 +8,8 @@ namespace GoXLR_Utility.NET.Commands.Mixer.Lighting.Simple
     {
         /// <summary>
         /// Set the Simple Colours<br/>
-        /// Like Accent, Scribble1, Scribble2, Scribble3, Scribble4, Global
+        /// Like Accent, Scribble1, Scribble2, Scribble3, Scribble4 <br/>
+        /// Global does not work.
         /// </summary>
         /// <param name="simple">The Simple one to change</param>
         /// <param name="colour1">The Colour 1 (#ffffff)</param>
